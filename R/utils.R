07fda7a1b0fba3ed7ce2651e82d8debdb1a4945d
mrkaye97 <mrkaye97@gmail.com>
2020-12-29 16:00:02 -0500

    force constants to upper case

--- R/utils.R
+++ R/utils.R
@@ -7,14 +7,14 @@
 get_constants <- function(const) {
   return(
     switch(
-      const,
-      "defense_positions" = c('CB', 'DB', 'DE', 'DL', 'FS', 'ILB', 'LB', 'MLB', 'NT', 'OLB', 'S', 'SS'),
-      "offense_positions" = c('QB', 'WR', 'TE', 'FB', 'HB', 'RB'),
-      "throw_start_events" = c('pass_forward', 'pass_shovel', 'pass_lateral'),
-      "throw_end_events" = c('pass_outcome_caught', 'pass_outcome_touchdown', 'pass_outcome_incomplete'),
-      "defensive_back_positions" = c('CB', 'DB', 'FS', 'S', 'SS'),
-      "linebacker_positions" = c('ILB', 'LB', 'MLB', 'OLB'),
-      "defensive_line_positions" = c('DE', 'DL', 'NT')
+      toupper(const),
+      "DEFENSE_POSITIONS" = c('CB', 'DB', 'DE', 'DL', 'FS', 'ILB', 'LB', 'MLB', 'NT', 'OLB', 'S', 'SS'),
+      "OFFENSE_POSITIONS" = c('QB', 'WR', 'TE', 'FB', 'HB', 'RB'),
+      "THROW_START_EVENTS" = c('pass_forward', 'pass_shovel', 'pass_lateral'),
+      "THROW_END_EVENTS" = c('pass_outcome_caught', 'pass_outcome_touchdown', 'pass_outcome_incomplete'),
+      "DEFENSIVE_BACK_POSITIONS" = c('CB', 'DB', 'FS', 'S', 'SS'),
+      "LINEBACKER_POSITIONS" = c('ILB', 'LB', 'MLB', 'OLB'),
+      "DEFENSIVE_LINE_POSITIONS" = c('DE', 'DL', 'NT')
     )
   )
 }
